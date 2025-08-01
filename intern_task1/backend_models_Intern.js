71fdea45e1c891e3c8b2c69563f097f2a82d4898
Archana Palanivel <archanapalanivel11@gmail.com>
2025-08-01 15:04:56 +0530

    Delete intern_task1 directory

--- intern_task1/backend_models_Intern.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const internSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    department: String,
-});
-
-module.exports = mongoose.model('Intern', internSchema);
